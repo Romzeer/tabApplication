c040bc351076215574e2162cd3fa4f4d88d7a328
D1nker <qfaure@gmail.com>
2018-01-10 17:59:20 +0100

    commit for Neo

---
+++ ThirdViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ThirdViewController.swift
+//  tabApp
+//
+//  Created by Quentin Faure on 10/01/2018.
+//  Copyright © 2018 Quentin Faure. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
