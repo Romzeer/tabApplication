ce3fb94ce33e15700f70a6589bd05f03e18d7f04
Laurent Maximin <laurent.maximin@eemi.com>
2018-01-17 14:45:48 +0100

    json ok

--- kituraPart/Sources/beacall-api/main.swift
+++ kituraPart/Sources/beacall-api/main.swift
@@ -1,13 +1,32 @@
 import Kitura
-import SwiftyJSON
 
 let router = Router()
 
 router.all("/", middleware: BodyParser(), StaticFileServer(path: "./Public"))
 
-router.get("/") { request, response, next in
+router.get("/:student") { request, response, next in
     
-    try response.send(["9h-11h": "Panoramas"]).end()
+    let studentName = request.parameters["student"] ?? ""
+    
+    if(studentName == "student1"){
+        try response.send(["course": "ruby",
+                           "start": "9.00am",
+                           "end": "11.00am",
+                           "latitude": "48.8688356",
+                           "longitude": "2.3414426",
+                           "range": "500meters"]).end()
+    }
+    else if(studentName == "student2"){
+        try response.send(["course": "market",
+                           "start": "9.00am",
+                           "end": "11.00am",
+                           "latitude": "48.8688356",
+                           "longitude": "2.3414426",
+                           "range": "500meters"]).end()
+    } else {
+        try response.redirect("/error").end()
+    }
+    
 }
 
 let port = 8080
